bcd75e25e93d0a434386cb75f70afa090a084de7
Kunstiception <lukas-bohl.de@outlook.de>
2024-07-22 00:05:14 +0200

    Türgriff animiert und Bücher verteilt

---
+++ Interactive3D_PA_Decker_Bohl/Assets/Scripts/Türklinke.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Türklinke : MonoBehaviour
+{
+    // Animator of the object
+    private Animator _animator;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _animator = gameObject.GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    public void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            _animator.SetTrigger("hasPushed");
+
+        }
+    }
+}
